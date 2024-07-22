fd3c7c364f21e65e4c45f62c3b0e832efa3c144f
pawel_dalecki <daleckipawel@proton.me>
2024-07-22 21:35:23 +0200

    Styling

--- main.js
+++ main.js
@@ -47,6 +47,14 @@ async function injectTemplate(origin, template, target, prepend) {
       : await fetch(`/public/templates/${template}.html`);
     const text = await res.text();
     const doc = new DOMParser().parseFromString(text, 'text/html');
+    if (doc.querySelector("style")) {
+      const style = doc.querySelector("style");
+      const css_rules = style.sheet.cssRules;
+      for (const rule of css_rules) {
+        console.log(rule.cssText)
+        document.styleSheets[0].insertRule(rule.cssText)
+      }
+    }
     if (target) {
       document.querySelector(target).append(...doc.children[0].querySelector("body").children);
     } else {
@@ -65,3 +73,22 @@ for (let template = 1; template <= currentTemplate.templates; template++) {
   await injectTemplate(currentTemplate.template, template, "main", false)
 }
 await injectTemplate(undefined, 'footer', undefined, false);
+
+const image = document.querySelector("#photo");
+
+function changePhoto() {
+  if (image.src.includes("magazynier")) {
+    image.src = "/public/images/lekarz.webp"
+  } else if (image.src.includes("lekarz")) {
+    image.src = "public/images/kucharz.webp"
+  } else if (image.src.includes("kucharz")) {
+    image.src = "public/images/nauczyciel.webp"
+  } else if (image.src.includes("nauczyciel")) {
+    image.src = "public/images/pracownik-biurowy.webp"
+  } else if (image.src.includes("pracownik-biurowy")) {
+    image.src = "public/images/magazynier.webp"
+  }
+}
+
+const interval = setInterval(changePhoto, 3000)
+
